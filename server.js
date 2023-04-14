4cd016dd5ecb8f70157e2e12dcf83039f9fdefd6
Alvaro Perez <japersa92@gmail.com>
2023-04-14 12:47:07 -0500

    second commit

--- server.js
+++ server.js
@@ -9,42 +9,40 @@ const websocket = socketio(server, {
     cors: true
   });
 
-startWebSocketServer();
+const clients = {};
+const users = {};
+let messages = [];
 
-function startWebSocketServer() {
-    const clients = {};
-    const users = {};
-
-    const sendMessage = (message) => {
-        websocket.emit('client:newNote', message);
-    }
-
-    websocket.on('connection', function (socket) {
-        clients[socket.id] = socket;
-
-        console.log('client connect...', socket.id);
-
-        socket.on('server:newNote', async (data) => {
-            console.log('message %s', data);
-            sendMessage(data);
-        })
-
-        socket.on('disconnect', async () => {
-            console.log('client disconnect...', socket.id)
-        })
-
-        socket.on('error', (err) => {
-            console.log('received error from client:', socket.id)
-            console.log(err)
-        })
-    })
-
-    websocket.on('connect', async (socket) => {
-        console.log(`User connect with the following id: ${socket.id}`);
-        users[socket.id] = socket;
-    });
+const sendMessage = () => {
+    websocket.emit('client:event', messages);
 }
 
+websocket.on('connection', function (socket) {
+    clients[socket.id] = socket;
+
+    console.log('client connect...', socket.id);
+
+    socket.on('server:event', async (data) => {
+        console.log('message %s', data);
+        messages.push(data)
+        sendMessage(data);
+    })
+
+    socket.on('disconnect', async () => {
+        console.log('client disconnect...', socket.id)
+    })
+
+    socket.on('error', (err) => {
+        console.log('received error from client:', socket.id)
+        console.log(err)
+    })
+})
+
+websocket.on('connect', async (socket) => {
+    console.log(`User connect with the following id: ${socket.id}`);
+    users[socket.id] = socket;
+    sendMessage();
+});
 
 const port = process.env.PORT_SOCKET || 9999;
 server.listen(port, function (err) {
